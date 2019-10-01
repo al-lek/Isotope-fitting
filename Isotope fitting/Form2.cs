7b42e8b2365b729426ba0c9272d8a92005398ba8
Lab <Lab@LAB-PC>
2019-10-01 18:06:35 +0300

    points' ruler annotation

--- Isotope fitting/Form2.cs
+++ Isotope fitting/Form2.cs
@@ -27,7 +27,6 @@ namespace Isotope_fitting
     public partial class Form2 : Form
     {
         #region parameter set
-
         bool plot_rem_Btns = false;
         bool refresh_all = false;
         int exp_res = 0;
@@ -106,8 +105,16 @@ namespace Isotope_fitting
         private bool is_calc = false;
 
         public static List<double[]> peak_points = new List<double[]>();
-
-
+        /// <summary>
+        /// previous clicked point on iso plot
+        /// </summary>
+        OxyPlot.ScreenPoint previous_point;
+        /// <summary>
+        /// distance between two points on screen
+        /// </summary>
+        double point_distance;
+        bool count_distance = false;
+        
         Stopwatch sw1 = new Stopwatch();
         Stopwatch sw2 = new Stopwatch();
         ProgressBar tlPrgBr;
@@ -1874,14 +1881,19 @@ namespace Isotope_fitting
             //////};
 
             iso_plot.Controller = new CustomPlotController();
-
             ContextMenu ctxMn = new ContextMenu() { };
             MenuItem showPoints = new MenuItem("Show charge ruler", manage_charge_points);
             MenuItem clearPoints = new MenuItem("Clear charge ruler", manage_charge_points);
             MenuItem copyImage = new MenuItem("Copy image", export_chartImage);
             MenuItem exportImage = new MenuItem("Export image to file", export_chartImage);
             ctxMn.MenuItems.AddRange(new MenuItem[] { showPoints, clearPoints, copyImage, exportImage });
+            
             iso_model.MouseDown += (s, e) => { if (e.ChangedButton == OxyMouseButton.Right) { charge_center = e.Position; ContextMenu = ctxMn; } };
+            iso_model.MouseDown += (s, e) => { if (e.ChangedButton == OxyMouseButton.Left && e.IsShiftDown == true) { if (count_distance) { cersor_distance(previous_point, e.Position); } else{ previous_point = e.Position; count_distance = true; } } else if(e.ChangedButton == OxyMouseButton.Left ){ count_distance = false;/* cersor_distance(e.Position, e.Position);*/ } };
+            iso_model.MouseMove += (s, e) => { if (count_distance && e.IsShiftDown == true) { cersor_distance(previous_point, e.Position); } else { cersor_distance(e.Position, e.Position); } };
+            iso_model.MouseUp += (s, e) => { count_distance = false;};            
+          
+
 
             //////iso_plot.MouseWheel += (s, e) => { if (e.Delta > 0 && e.ToMouseEventArgs(OxyModifierKeys.Control).IsControlDown) iso_model.DefaultXAxis.ZoomAtCenter(2) ; };
             //////bool isControlDown = System.Windows.Input Keyboard.IsKeyDown(Key.LeftCtrl);
@@ -1920,7 +1932,36 @@ namespace Isotope_fitting
             //iso_plot.MouseHover += (s, e) => { iso_plot.Focus(); };
             //res_plot.MouseHover += (s, e) => { res_plot.Focus(); };
         }
+        private void cersor_distance(ScreenPoint a, ScreenPoint b)
+        {
+            iso_plot.Model.Annotations.Clear();            
+            double mz_a = iso_plot.Model.DefaultXAxis.InverseTransform(a.X);
+            double h_a = iso_plot.Model.DefaultYAxis.InverseTransform(a.Y);
+            double min_border = mz_a;
+            double max_border = mz_a;            
+            if (count_distance)
+            {
+                double mz_b = iso_plot.Model.DefaultXAxis.InverseTransform(b.X);
+                point_distance = Math.Abs(mz_a - mz_b);
+                iso_plot.Model.Annotations.Add(new PointAnnotation() { X = mz_a , Y = h_a, Size = 4, Text = "distance: " + Math.Round(Math.Abs(point_distance), 4).ToString(), Fill = OxyColors.Black, Shape=MarkerType.None,TextVerticalAlignment = VerticalAlignment.Top });
+                iso_plot.Model.Annotations.Add( new LineAnnotation { X = mz_a, Type = LineAnnotationType.Vertical} );
+                iso_plot.Model.Annotations.Add(new LineAnnotation { Y = h_a, Type = LineAnnotationType.Horizontal });
 
+                //iso_plot.Model.Annotations.Add(new RectangleAnnotation { MinimumX = min_border, MaximumX = min_border+ point_distance, Fill = OxyColor.FromAColor(99, OxyColors.Gainsboro) });
+                iso_plot.Model.Annotations.Add(new RectangleAnnotation { MinimumX = min_border, MaximumX = min_border + point_distance, Fill = OxyColor.FromAColor(99, OxyColors.LightSalmon) });
+
+            }
+            else
+            {
+                iso_plot.Model.Annotations.Add(new PointAnnotation() { X = mz_a, Y = h_a, Size = 2, Text = "int:" + Math.Round(h_a, 4).ToString() + " , " + "m/z:" + Math.Round(mz_a, 4).ToString(), Fill = OxyColors.Black, Shape = MarkerType.None, TextVerticalAlignment = VerticalAlignment.Top });
+                //iso_plot.Model.Annotations.Add(new PointAnnotation() { X = mz_a, Y = h_a-2, Size = 4, Text = "m/z:" + Math.Round(mz_a, 4).ToString(),Fill=OxyColors.Transparent, Shape = MarkerType.Plus, TextVerticalAlignment = VerticalAlignment.Top });
+                iso_plot.Model.Annotations.Add(new LineAnnotation { X = mz_a, Type = LineAnnotationType.Vertical });
+                iso_plot.Model.Annotations.Add(new LineAnnotation { Y = h_a, Type = LineAnnotationType.Horizontal });
+
+            }
+
+            invalidate_all();
+        }
         public class CustomPlotController : PlotController
         {
             public CustomPlotController()
