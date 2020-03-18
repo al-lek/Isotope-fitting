3780d071ee5d882fe0cde711b70f5881ff13afd2
Maro <maro.kouvaraki@fasmatech.com>
2020-03-18 17:50:52 +0200

    form16

--- Isotope fitting/Form16.Designer.cs
+++ Isotope fitting/Form16.Designer.cs
@@ -37,6 +37,7 @@
             this.heavy_BoxFrm16 = new System.Windows.Forms.RichTextBox();
             this.light_chain_tab = new System.Windows.Forms.TabPage();
             this.light_BoxFrm16 = new System.Windows.Forms.RichTextBox();
+            this.add_tab_page = new System.Windows.Forms.TabPage();
             this.seq_tabControl.SuspendLayout();
             this.seq_tab.SuspendLayout();
             this.heavy_chain_tab.SuspendLayout();
@@ -45,15 +46,15 @@
             // 
             // seq_Btn
             // 
+            this.seq_Btn.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.seq_Btn.BackColor = System.Drawing.Color.ForestGreen;
             this.seq_Btn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.seq_Btn.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.seq_Btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.seq_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.seq_Btn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.seq_Btn.Location = new System.Drawing.Point(0, 304);
+            this.seq_Btn.Location = new System.Drawing.Point(575, 335);
             this.seq_Btn.Name = "seq_Btn";
-            this.seq_Btn.Size = new System.Drawing.Size(687, 32);
+            this.seq_Btn.Size = new System.Drawing.Size(119, 32);
             this.seq_Btn.TabIndex = 1;
             this.seq_Btn.Text = "Save Sequence ";
             this.seq_Btn.UseVisualStyleBackColor = false;
@@ -67,7 +68,7 @@
             this.seq_BoxFrm16.Location = new System.Drawing.Point(3, 3);
             this.seq_BoxFrm16.Name = "seq_BoxFrm16";
             this.seq_BoxFrm16.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.seq_BoxFrm16.Size = new System.Drawing.Size(673, 272);
+            this.seq_BoxFrm16.Size = new System.Drawing.Size(692, 168);
             this.seq_BoxFrm16.TabIndex = 2;
             this.seq_BoxFrm16.Text = "";
             this.seq_BoxFrm16.TextChanged += new System.EventHandler(this.seq_BoxFrm16_TextChanged);
@@ -77,11 +78,12 @@
             this.seq_tabControl.Controls.Add(this.seq_tab);
             this.seq_tabControl.Controls.Add(this.heavy_chain_tab);
             this.seq_tabControl.Controls.Add(this.light_chain_tab);
-            this.seq_tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.seq_tabControl.Controls.Add(this.add_tab_page);
+            this.seq_tabControl.Dock = System.Windows.Forms.DockStyle.Top;
             this.seq_tabControl.Location = new System.Drawing.Point(0, 0);
             this.seq_tabControl.Name = "seq_tabControl";
             this.seq_tabControl.SelectedIndex = 0;
-            this.seq_tabControl.Size = new System.Drawing.Size(687, 304);
+            this.seq_tabControl.Size = new System.Drawing.Size(706, 200);
             this.seq_tabControl.TabIndex = 3;
             // 
             // seq_tab
@@ -90,7 +92,7 @@
             this.seq_tab.Location = new System.Drawing.Point(4, 22);
             this.seq_tab.Name = "seq_tab";
             this.seq_tab.Padding = new System.Windows.Forms.Padding(3);
-            this.seq_tab.Size = new System.Drawing.Size(679, 278);
+            this.seq_tab.Size = new System.Drawing.Size(698, 174);
             this.seq_tab.TabIndex = 0;
             this.seq_tab.Text = "General Sequence";
             this.seq_tab.UseVisualStyleBackColor = true;
@@ -101,7 +103,7 @@
             this.heavy_chain_tab.Location = new System.Drawing.Point(4, 22);
             this.heavy_chain_tab.Name = "heavy_chain_tab";
             this.heavy_chain_tab.Padding = new System.Windows.Forms.Padding(3);
-            this.heavy_chain_tab.Size = new System.Drawing.Size(776, 303);
+            this.heavy_chain_tab.Size = new System.Drawing.Size(698, 174);
             this.heavy_chain_tab.TabIndex = 1;
             this.heavy_chain_tab.Text = "Heavy Chain";
             this.heavy_chain_tab.UseVisualStyleBackColor = true;
@@ -114,7 +116,7 @@
             this.heavy_BoxFrm16.Location = new System.Drawing.Point(3, 3);
             this.heavy_BoxFrm16.Name = "heavy_BoxFrm16";
             this.heavy_BoxFrm16.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.heavy_BoxFrm16.Size = new System.Drawing.Size(770, 297);
+            this.heavy_BoxFrm16.Size = new System.Drawing.Size(692, 168);
             this.heavy_BoxFrm16.TabIndex = 3;
             this.heavy_BoxFrm16.Text = "";
             this.heavy_BoxFrm16.TextChanged += new System.EventHandler(this.heavy_BoxFrm16_TextChanged);
@@ -125,7 +127,7 @@
             this.light_chain_tab.Location = new System.Drawing.Point(4, 22);
             this.light_chain_tab.Name = "light_chain_tab";
             this.light_chain_tab.Padding = new System.Windows.Forms.Padding(3);
-            this.light_chain_tab.Size = new System.Drawing.Size(776, 303);
+            this.light_chain_tab.Size = new System.Drawing.Size(698, 174);
             this.light_chain_tab.TabIndex = 2;
             this.light_chain_tab.Text = "Light Chain";
             this.light_chain_tab.UseVisualStyleBackColor = true;
@@ -138,16 +140,26 @@
             this.light_BoxFrm16.Location = new System.Drawing.Point(3, 3);
             this.light_BoxFrm16.Name = "light_BoxFrm16";
             this.light_BoxFrm16.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.light_BoxFrm16.Size = new System.Drawing.Size(770, 297);
+            this.light_BoxFrm16.Size = new System.Drawing.Size(692, 168);
             this.light_BoxFrm16.TabIndex = 3;
             this.light_BoxFrm16.Text = "";
             this.light_BoxFrm16.TextChanged += new System.EventHandler(this.light_BoxFrm16_TextChanged);
+            // 
+            // add_tab_page
+            // 
+            this.add_tab_page.Location = new System.Drawing.Point(4, 22);
+            this.add_tab_page.Name = "add_tab_page";
+            this.add_tab_page.Padding = new System.Windows.Forms.Padding(3);
+            this.add_tab_page.Size = new System.Drawing.Size(698, 174);
+            this.add_tab_page.TabIndex = 3;
+            this.add_tab_page.Text = "+";
+            this.add_tab_page.UseVisualStyleBackColor = true;
             // 
             // Form16
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(687, 336);
+            this.ClientSize = new System.Drawing.Size(706, 379);
             this.Controls.Add(this.seq_tabControl);
             this.Controls.Add(this.seq_Btn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -175,5 +187,6 @@
         private System.Windows.Forms.TabPage light_chain_tab;
         private System.Windows.Forms.RichTextBox heavy_BoxFrm16;
         private System.Windows.Forms.RichTextBox light_BoxFrm16;
+        private System.Windows.Forms.TabPage add_tab_page;
     }
 }
---
+++ Isotope fitting/SequenceTab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isotope_fitting
+{
+    class SequenceTab
+    {
+        private string sequence;
+        private string extension;
+        private int type;
+        public int Type
+        {
+            get { return this.type; }
+            set { this.type = value; }
+        }
+        public string Extension
+        {
+            get { return this.extension; }
+            set { this.extension = value; }
+        }
+        public string Sequence
+        {
+            get { return this.sequence; }
+            set { this.sequence = value; }
+        }
+    }
+}
